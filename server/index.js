2a9500b5d5170429f0e3fa10c75e2145657b337c
tfprome <tfpromy99@gmail.com>
2025-09-10 19:59:41 +0600

    updated gitignore

--- server/index.js
+++ server/index.js
@@ -68,8 +68,8 @@ mongoose.connect(process.env.MONGO_URI, {
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.error("❌ MongoDB connection error:", err));
 
-// Server
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
 });
