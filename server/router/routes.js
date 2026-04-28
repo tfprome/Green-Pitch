856005a6eaef25ef22cb83acedea11dbb67ce5fe
tfprome <tfpromy99@gmail.com>
2026-04-28 11:57:01 +0600

    getproducts api fix

--- server/router/routes.js
+++ server/router/routes.js
@@ -1,5 +1,6 @@
-import express from'express'
-const router=express.Router();
+import express from 'express'
+const router = express.Router();
+import upload from '../middleware/multermiddleware.js';
 
 
 import * as homecontroller from '../controllers/homecontrollers.js'
@@ -9,43 +10,45 @@ import * as productcontrollers from '../controllers/productcontrollers.js'
 import * as usercontrollers from '../controllers/usercontrollers.js'
 import * as cartcontrollers from '../controllers/cartcontrollers.js'
 import { authmiddleware } from '../middleware/authmiddleware.js';
-import * as wishcontrollers from  '../controllers/wishcontrollers.js'
+import * as wishcontrollers from '../controllers/wishcontrollers.js'
 import * as invoicecontrollers from '../controllers/invoicecontrollers.js';
 //import createinvoiceservice from './../controllers/invoicecontrollers.js';
 
 
-router.get('/slider',homecontroller.Slidercontroller)
-router.get('/brand',homecontroller.Brandshowcontroller)
-router.get('/category',homecontroller.Categoryshowcontroller)
-router.get('/teamname',homecontroller.teamname)
-router.get('/brandname',homecontroller.brandname)
-router.get('/categoryname',homecontroller.categoryname)
+router.get('/slider', homecontroller.Slidercontroller)
+router.get('/brand', homecontroller.Brandshowcontroller)
+router.get('/category', homecontroller.Categoryshowcontroller)
+router.get('/teamname', homecontroller.teamname)
+router.get('/brandname', homecontroller.brandname)
+router.get('/categoryname', homecontroller.categoryname)
 
-router.post('/signup',usercontrollers.signupservice)
-router.post('/login',usercontrollers.loginservice)
+router.post('/signup', usercontrollers.signupservice)
+router.post('/login', usercontrollers.loginservice)
 
-router.get('/getproducts',authmiddleware,productcontrollers.getproducts)
-router.get('/productdetails/:id',productcontrollers.detailsService)
-router.get('/listbycategory/:id',productcontrollers.listbycategoryservice)
-router.get('/listbybrand/:id',productcontrollers.listbybrandservice)
-router.get('/productlistbykeyword/:value',productcontrollers.productlistbykeyword)
+router.get('/getproducts', authmiddleware, productcontrollers.getproducts)
+router.get('/productdetails/:id', productcontrollers.detailsService)
+router.get('/listbycategory/:id', productcontrollers.listbycategoryservice)
+router.get('/listbybrand/:id', productcontrollers.listbybrandservice)
+router.get('/productlistbykeyword/:value', productcontrollers.productlistbykeyword)
 
-router.post('/addcart',authmiddleware,cartcontrollers.addcartservice)
-router.get('/readcart',authmiddleware,cartcontrollers.readcartsevice)
-router.delete('/removecart/:id',authmiddleware,cartcontrollers.removecartservice)
+router.post('/addcart', authmiddleware, cartcontrollers.addcartservice)
+router.get('/readcart', authmiddleware, cartcontrollers.readcartsevice)
+router.delete('/removecart/:id', authmiddleware, cartcontrollers.removecartservice)
 
 
-router.post('/addwish/:id',authmiddleware,wishcontrollers.addwishservice)
-router.get('/readwish',authmiddleware,wishcontrollers.readwishservice)
-router.delete('/removewish/:id',authmiddleware,wishcontrollers.removewishservice)
+router.post('/addwish/:id', authmiddleware, wishcontrollers.addwishservice)
+router.get('/readwish', authmiddleware, wishcontrollers.readwishservice)
+router.delete('/removewish/:id', authmiddleware, wishcontrollers.removewishservice)
 
-router.post('/createinvoice/:id',authmiddleware,invoicecontrollers.createinvoiceservice)
-router.get('/invoice/:id',authmiddleware,invoicecontrollers.getInvoiceDetails)
+router.post('/createinvoice/:id', authmiddleware, invoicecontrollers.createinvoiceservice)
+router.get('/invoice/:id', authmiddleware, invoicecontrollers.getInvoiceDetails)
 router.post("/payment/initiate/:id", authmiddleware, invoicecontrollers.initiatePayment);
 router.get("/payment-success/:id", invoicecontrollers.paymentSuccess);
 router.get("/payment-fail/:id", invoicecontrollers.paymentFail);
 
-router.get('/user',authmiddleware,usercontrollers.GetIndividualUser)
+router.get('/user', authmiddleware, usercontrollers.GetIndividualUser)
+
+router.post('/admin/addproduct',authmiddleware,upload.single('brandimg'),productcontrollers.addproduct)
 
 
 export default router;
